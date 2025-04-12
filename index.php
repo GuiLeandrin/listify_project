cb57b2f3bd3a54c10704426079a1cdd926ae0672
Guilherme Leandrin Fernandes <guileandrin43@gmail.com>
2025-04-11 23:05:39 -0300

    Sistema_Login_Organization

--- index.php
+++ index.php
@@ -1,20 +1,25 @@
 <?php
     session_start();
-    $erroEmail = @$_SESSION['erroEmail'];
     $erroSenha = @$_SESSION['erroSenha'];
     $erroForms = @$_SESSION['erroForms'];
     $erroConta = @$_SESSION['erroConta'];
-    unset($_SESSION['erroEmail'], $_SESSION['erroSenha'], $_SESSION['erroForms'], $_SESSION['erroConta']);
+    unset($_SESSION['erroSenha'], $_SESSION['erroForms'], $_SESSION['erroConta']);
+
     if(isset($_POST['submit'])) {
         $conexao = new mysqli("localhost", "root", "", "website");
         $email = @$_POST['email'];
         $senha = @$_POST['senha'];
         $msg = false;
-        
+
         $sql = "SELECT * FROM usuarios WHERE email = '$email' AND senha = '$senha';";
         $verifica = $conexao->query($sql);
+
         if($email && $senha) {
             if ($verifica->num_rows > 0) {
+                $usuario = $verifica->fetch_assoc();
+                $_SESSION['id'] = $usuario['id'];
+                $_SESSION['email'] = $usuario['email'];
+                
                 header("Location: xxx.xxx");
                 exit;
             } else {
@@ -26,28 +31,27 @@
                     $msg = true;
                     if($msg) {
                         $erroConta = "
-                            <div class='bg-danger mt-4 rounded text-center w-100 p-2 mt-1'>
-                                <p class='text-white m-0 d-flex justify-content-center align-items-center h-100'>Este e-mail não possui uma conta!!</p>
+                            <div class='mt-4 rounded text-center w-100 p-2 mt-1' style='background-color: #f8d7da; color: #721c24;'>
+                                <p class='m-0 d-flex justify-content-center align-items-center h-100'>Este E-mail não possui uma conta!!</p>
                             </div>
                         ";
                     }
                     $msg = false;
                 } elseif ($verificaSenha->num_rows == 0) {
-                    $erroSenha = "<p class='text-center p-2 mt-1 bg-danger w-75 text-white rounded'>Senha incorreta, digite novamente!</p>";
+                    $erroSenha = "<p class='text-center p-2 mt-1 w-75 rounded' style='background-color: #f8d7da; color: #721c24;'>Senha incorreta, digite novamente!</p>";
                 }
             }
         } else {    
             $msg = true;
             if($msg) {
                 $erroForms = "
-                    <div class='bg-danger mt-4 rounded text-center w-100 p-2 mt-1'>
-                        <p class='text-white m-0 d-flex justify-content-center align-items-center h-100'>Formulário não preenchido corretamente!!</p>
+                    <div class='mt-4 rounded text-center w-100 p-2 mt-1' style='background-color: #f8d7da; color: #721c24;'>
+                        <p class='m-0 d-flex justify-content-center align-items-center h-100'>Formulário não preenchido corretamente!!</p>
                     </div>
                 ";
             }
             $msg = false;
         }
-        $_SESSION['erroEmail'] = $erroEmail;
         $_SESSION['erroSenha'] = $erroSenha;
         $_SESSION['erroForms'] = $erroForms;
         $_SESSION['erroConta'] = $erroConta;
@@ -65,41 +69,38 @@
     <title>Exercício Login - Bootstrap</title>
 </head>
 <body class="vh-100 d-flex align-items-center justify-content-center p-2 row overflow-hidden" style="background: linear-gradient(45deg, #f0f0f0, #d6d6d6);">
-    <div class="bg-white shadow-lg rounded-5 d-flex flex-column align-items-center col-md-4 col-sm-8 col-10" style="min-height: 350px; padding: 2rem 0;">
-        <div class="w-75 mb-3 pb-4 d-flex align-items-center justify-content-center border-bottom border-5 border-black">
+    <div class="bg-white shadow-lg rounded-5 d-flex flex-column align-items-center col-md-4 col-sm-8 col-10" style="min-height: 450px; padding: 2rem 0;">
+        <div class="w-75 mb-4 pb-4 d-flex align-items-center justify-content-center border-bottom border-5 border-black">
                 <h1>Log in</h1>
         </div>
-            <form action=""  method="POST" class="w-100 h-75 d-flex flex-column align-items-center">
-                <div class="input-group rounded-1 border border-2 border-dark border-opacity-25 w-75 mt-4">
-                    <label for="email" class="input-group-text bg-transparent border-0 text-decoration-none"  style="cursor: text;"><i class="fa-solid fa-envelope"></i></label>
-                    <input type="email" class="form-control bg-transparent border-0 shadow-none" placeholder="Digite seu e-mail:" name="email" id="email">
-                </div>
-                <?php if ($erroEmail): ?>
-                        <?php echo $erroEmail; ?>
+        <form action=""  method="POST" class="w-100 h-75 d-flex flex-column align-items-center">
+            <div class="input-group rounded-1 border border-2 border-dark border-opacity-25 w-75 mt-4">
+                <label for="email" class="input-group-text bg-transparent border-0 text-decoration-none"  style="cursor: text;"><i class="fa-solid fa-envelope"></i></label>
+                <input type="email" class="form-control bg-transparent border-0 shadow-none" placeholder="Digite seu e-mail:" name="email" id="email">
+            </div>
+            <div class="input-group rounded-1 border border-2 border-dark border-opacity-25 w-75 mt-3 mb-2">
+                <label for="senha" class="input-group-text bg-transparent border-0 text-decoration-none"  style="cursor: text;"><i class="fa-solid fa-lock"></i></label>
+                <input type="password" class="form-control bg-transparent border-0 shadow-none" placeholder="Digite sua senha:" name="senha" id="senha">
+            </div>
+            <?php if ($erroSenha): ?>
+                    <?php echo $erroSenha; ?>
+            <?php endif; ?>
+            <div class="w-75 mt-4 d-flex gap-1">
+                <input type="submit" name="submit" class="btn btn-primary w-100" value="Entrar">
+                <a href="cadastro.php" class="btn btn-primary w-100">Cadastro</a>
+            </div>
+            <div class="w-75 d-flex gap-1">
+                <?php if ($erroForms): ?>
+                        <?php echo $erroForms; ?>
                 <?php endif; ?>
-                <div class="input-group rounded-1 border border-2 border-dark border-opacity-25 w-75 mt-3 mb-2">
-                    <label for="senha" class="input-group-text bg-transparent border-0 text-decoration-none"  style="cursor: text;"><i class="fa-solid fa-lock"></i></label>
-                    <input type="password" class="form-control bg-transparent border-0 shadow-none" placeholder="Digite sua senha:" name="senha" id="senha">
-                </div>
-                <?php if ($erroSenha): ?>
-                        <?php echo $erroSenha; ?>
+                <?php if ($erroConta): ?>
+                        <?php echo $erroConta; ?>
                 <?php endif; ?>
-                <div class="w-75 mt-4 d-flex gap-1">
-                    <input type="submit" name="submit" class="btn btn-primary w-100" value="Entrar">
-                    <a href="cadastro.php" class="btn btn-primary w-100">Cadastro</a>
-                </div>
-                <div class="w-75 d-flex gap-1">
-                    <?php if ($erroForms): ?>
-                            <?php echo $erroForms; ?>
-                    <?php endif; ?>
-                    <?php if ($erroConta): ?>
-                            <?php echo $erroConta; ?>
-                    <?php endif; ?>
-                    </div>
-                <div class="mt-4">
-                    <h6><a href="">Esqueceu sua Senha?</a></h6>
-                </div>
-            </form>
+            </div>
+            <div class="mt-5">
+                <h6><a href="">Esqueceu sua Senha?</a></h6>
+            </div>
+        </form>
     </div>
 </body>
 </html>
